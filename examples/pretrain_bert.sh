1eefd9c4ec4961069c1b95b0c178a41b3389f147
fanxinli <fanxin.li@outlook.com>
2023-03-09 16:40:49 +0000

    update

--- examples/pretrain_bert.sh
+++ examples/pretrain_bert.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
 
-GPUS_PER_NODE=8
-# Change for multinode config
-MASTER_ADDR=localhost
-MASTER_PORT=6000
-NNODES=1
-NODE_RANK=0
-WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+cat bert_text_sentence.bin.* > bert_text_sentence.bin
 
-DATA_PATH=<Specify path and file prefix>_text_sentence
-VOCAB_FILE=<Specify path to vocab.txt>
-CHECKPOINT_PATH=<Specify path>
+DATA_PATH=bert_text_sentence
+CMD="python -u pretrain_bert.py \
+     --use-new-schedule \
+     --num-workers 8 \
+     --activations-checkpoint-method uniform \
+     --activations-checkpoint-num-layers 1 \
+     --pipeline-model-parallel-size 4 \
+     --num-layers-per-virtual-pipeline-stage 2 \
+     --tensor-model-parallel-size 4 \
+     --num-layers 32 \
+     --hidden-size 4096 \
+     --num-attention-heads 32 \
+     --micro-batch-size 8 \
+     --global-batch-size 128 \
+     --seq-length 1024 \
+     --max-position-embeddings 1024 \
+     --train-iters 1000000 \
+     --data-path $DATA_PATH \
+     --vocab-file vocab.txt \
+     --data-impl mmap \
+     --split 949,50,1 \
+     --distributed-backend nccl \
+     --lr 0.0001 \
+     --lr-decay-style linear \
+     --min-lr 1.0e-5 \
+     --lr-decay-iters 990000 \
+     --weight-decay 1e-2 \
+     --clip-grad 1.0 \
+     --lr-warmup-fraction .01 \
+     --log-interval 10 \
+     --exit-interval 40 \
+     --save-interval 10000 \
+     --eval-interval 1000 \
+     --eval-iters 10 \
+     --fp16"
 
-DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+THIS_DIR=`pwd`
+DATETIME=`date +'date_%y-%m-%d_time_%H-%M-%S'`
+LOG_DIR=${THIS_DIR}/logs/$DATETIME
+mkdir -p ${LOG_DIR}
+cp $0 ${LOG_DIR}/run.sh
+echo Commit-Hash `git rev-parse --short HEAD` >> ${LOG_DIR}/version.log
 
-python -m torch.distributed.launch $DISTRIBUTED_ARGS \
-       pretrain_bert.py \
-       --tensor-model-parallel-size 2 \
-       --pipeline-model-parallel-size 2 \
-       --num-layers 24 \
-       --hidden-size 1024 \
-       --num-attention-heads 16 \
-       --micro-batch-size 2 \
-       --global-batch-size 16 \
-       --seq-length 512 \
-       --max-position-embeddings 512 \
-       --train-iters 1000000 \
-       --save $CHECKPOINT_PATH \
-       --load $CHECKPOINT_PATH \
-       --data-path $DATA_PATH \
-       --vocab-file $VOCAB_FILE \
-       --data-impl mmap \
-       --split 949,50,1 \
-       --distributed-backend nccl \
-       --lr 0.0001 \
-       --lr-decay-style linear \
-       --min-lr 1.0e-5 \
-       --lr-decay-iters 990000 \
-       --weight-decay 1e-2 \
-       --clip-grad 1.0 \
-       --lr-warmup-fraction .01 \
-       --log-interval 100 \
-       --save-interval 10000 \
-       --eval-interval 1000 \
-       --eval-iters 10 \
-       --fp16
+srun --output=${LOG_DIR}/%j.%t.log \
+     -p INTERN -N1 -n8 --ntasks-per-node=8 --gres=gpu:8 \
+     --kill-on-bad-exit=1 \
+     sh -c "${CMD}"
